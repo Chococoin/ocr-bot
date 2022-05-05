7d21fde492de0906f5923b9c9583cca9fc3ea6b3
Germán Lugo <g.lugo.dev@gmail.com>
2022-05-05 12:28:21 +0200

    Initial commit.

---
+++ main.js
@@ -0,0 +1,30 @@
+const { Telegraf } = require('telegraf')
+const { session } = require('telegraf')
+const { Scenes } = require('telegraf')
+const { Markup } = require('telegraf')
+require('dotenv').config()
+
+const imageScene = require('./scenes/imageScene').imageScene
+const videoScene = require('./scenes/videoScene').videoScene
+
+const bot = new Telegraf(process.env.BOT_TOKEN)
+const stage = new Scenes.Stage([imageScene, videoScene])
+bot.use(session())
+bot.use(stage.middleware())
+
+bot.start((ctx) => {
+  let userFirstName = ctx.message.from.first_name
+  let message = ` Hello master ${userFirstName}, i am OCR bot your humble servant. \n
+  Where would you like to extract text from ?`
+
+  let options = Markup.inlineKeyboard([
+      Markup.button.callback('Extract from 🖼️', 'extractFromImage'),
+      Markup.button.callback('Extract from 🎬', 'extractFromVideo'),
+  ])
+  ctx.reply(message, options)
+})
+
+bot.action('extractFromImage', Scenes.Stage.enter('imageScene'))
+bot.action('extractFromVideo', Scenes.Stage.enter('videoScene'))
+
+bot.launch()
